4e3fc8d5db1c2d1e7935e6ec5d59588e1d104755
numeshj <numeshjayamanne@gmail.com>
2025-06-12 15:59:12 +0530

    URL Base5

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // <-- Add Routes and Route
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom"; // <-- Add Routes and Route
 import "./index.css";
 import App from "./App.jsx";
 import ASG_1 from "./pages/ASG_1.jsx";
@@ -9,13 +9,13 @@ import ASG_2 from "./pages/ASG_2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/asg-1" element={<ASG_1 />} />
         <Route path="/asg-2" element={<ASG_2 />} />
         <Route path="/asg-3" element={<ASG_3 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
